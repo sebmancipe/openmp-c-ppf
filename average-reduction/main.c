0cb0f0649a2c0199b8d5958d650563f241288d61
anderballes98 <ander.ballesteros98@gmail.com>
2020-10-08 16:56:48 -0500

    add prints guide avg-reduction

--- average-reduction/main.c
+++ average-reduction/main.c
@@ -10,10 +10,12 @@ int main() {
 // Esta funcion explica explisitamente cómo trabaja la funcion reduction de omp
 void avg_round_robin() {
     int N = 600000000;
+    // int N = 20;
     double tavg = 0;
 
     double timer_start = omp_get_wtime();
-    omp_set_num_threads(16);
+    // omp_set_num_threads(16);
+    omp_set_num_threads(5);
 
     // Inicia la explicacion de la función reduction
     #pragma omp parallel // Inicio de zona paralela
@@ -24,6 +26,7 @@ void avg_round_robin() {
 
         for (int i = id; i < N; i+=nthreads) { // asignación de la distribución de trabajo para cada hilo. Ej: Nthreads=4; thread0=(0, 4, 8...); thread3=(3, 7, 11...) 
             avg += i;
+            // printf("thread: %d, %.2f\n", id, avg);
         }
         #pragma omp atomic
         tavg += avg; // Al finalizar la ejecución de cada hilo, su valor final se agrega a la variable global
@@ -38,16 +41,19 @@ void avg_round_robin() {
 
 void avg_reduction() {
     int N = 600000000;
+    // int N = 20;
     int j = 0;
     double tavg = 0;
 
     double timer_start = omp_get_wtime();
     omp_set_num_threads(48);
+    // omp_set_num_threads(5);
 
     // Explicación detallada en la función avg_round_robin() 
     #pragma omp parallel for reduction(+:tavg)
         for (j = 0; j < N; ++j) {
             tavg += j;
+            // printf("thread: %d, %.2f\n", omp_get_thread_num(), tavg);
         }
 
     double timer_elapsed = omp_get_wtime() - timer_start;
